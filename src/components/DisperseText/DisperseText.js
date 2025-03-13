53621347394476974a875607d666c14eb4f31274
phantomcorn <jj.phantakorn@outlook.com>
2025-03-13 22:40:04 +0700

    DisperseText component

---
+++ src/components/DisperseText/DisperseText.js
@@ -0,0 +1,35 @@
+"use client"
+import {motion} from "framer-motion"
+import { useState } from "react"
+import { disperse } from "./animation"
+
+import style from "./DisperseText.module.css"
+
+export default function TextDisperse({children, setBackground, variant = "variant1"}) {
+
+    const [isAnimated, setIsAnimated] = useState(false)
+
+    const getChars = (text) => {
+        let chars = []
+        text.split("").forEach((char, i) => {
+            chars.push(<motion.span custom={i} variants={disperse} animate={isAnimated? "open" : "closed"} key={char + i}>{char}</motion.span>)
+        })
+        return chars
+    }
+
+    const onMouseEnter = (e) => {
+        setIsAnimated(true)
+        setBackground(true)
+    }
+
+    const onMouseLeave = (e) => {
+        setIsAnimated(false)
+        setBackground(false)
+    }
+
+    return (
+        <div className={`${style[variant]} ${style.media}`} onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave}>
+            {getChars(children)}
+        </div>
+    )
+}
