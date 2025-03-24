68ad4d5b39378a7629dbc1d860f75d284399fbf8
phantomcorn <jj.phantakorn@outlook.com>
2025-03-24 21:15:04 +0700

    Button with back arrow option

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,10 +1,14 @@
 import styles from "./Button.module.css";
+import ArrowSVG from "@/../public/arrow.svg"
+import Image from "next/image";
+export default function Button({ variant = "color1", children, back = false, ...props}) {
 
-export default function Button({ variant = "color1", children, ...props}) {
 
     return (
-        <div {...props} className={`${styles.button} ${styles[variant]}`}>
+        <div {...props} className={`${styles.button} ${styles[variant]} ${(children || back) ? styles.select : styles.none}`}>
+
             {children}
+            {back && <Image src={ArrowSVG} alt="back-arrow"/>}
         </div>
     )
 }
