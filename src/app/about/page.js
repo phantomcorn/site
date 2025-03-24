be83f913b3133c32e0f6b233070eea208eb232c6
phantomcorn <jj.phantakorn@outlook.com>
2025-03-24 21:08:16 +0700

    add media and use map instead

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -6,19 +6,44 @@ import gsap from "gsap"
 import { useRef } from "react"
 
 export default function About() {
-
+    
     const backgroundRef = useRef(null)
+
     const setBackground = (isActive) => {
         gsap.to(backgroundRef.current, {opacity: isActive? 0.8 : 0})
     }
+
+    const media = [
+        { 
+            alt: "Github",
+            link: "https://github.com/phantomcorn/"
+        },
+        { 
+            alt: "LinkedIn",
+            link: "https://www.linkedin.com/in/phantakorn-prarusudamkerng-6bb73b207/"
+        },
+        { 
+            alt: "Instagram",
+            link: "https://www.instagram.com/jaywithnoay/"
+        },
+        { 
+            alt: "Email",
+            link: "https://github.com/phantomcorn/"
+        }
+    ]
+
+    const onClick = (e, link) => {
+        e.preventDefault()
+        window.open(link, "_blank");
+    }
+
     return (
         <div className={styles.page}>
             <Card/>
             <div className={styles["media-container"]}>
-                <TextDisperse variant="variant1" setBackground={setBackground}>Github</TextDisperse>
-                <TextDisperse variant="variant2" setBackground={setBackground}>LinkedIn</TextDisperse>
-                <TextDisperse variant="variant3" setBackground={setBackground}>Instagram</TextDisperse>
-                <TextDisperse variant="variant4" setBackground={setBackground}>Email</TextDisperse>
+                {media.map((item, i) => 
+                    <TextDisperse variant={`variant${i + 1}`} key={`media${i+1}`} onClick={(e) => onClick(e, item.link)} setBackground={setBackground}>{item.alt}</TextDisperse>)
+                }
                 <div ref={backgroundRef} className={styles.background}></div>
             </div>
         </div>
