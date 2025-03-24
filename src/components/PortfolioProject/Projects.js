8c05d3f46228f615e1d161a08e44eafc4b86ef82
phantomcorn <jj.phantakorn@outlook.com>
2025-03-24 21:15:56 +0700

    Projects

---
+++ src/components/PortfolioProject/Projects.js
@@ -0,0 +1,35 @@
+
+const projects = {
+    "oasis-residence": {
+        type: "web",
+        media: ["https://res.cloudinary.com/dmg2dllnz/image/upload/v1742792906/1_usltrk.png",
+                "https://res.cloudinary.com/dmg2dllnz/image/upload/v1742792905/2_ffv6vt.png",
+                "https://res.cloudinary.com/dmg2dllnz/image/upload/v1742792906/3_v59jyd.png",
+                "https://res.cloudinary.com/dmg2dllnz/image/upload/v1742792904/4_mkamff.png"],
+        width: 3840,
+        height: 2240,
+        alt: "Oasis residence media",
+        source: "https://oasisresidences.netlify.app/"
+    },
+    "tonamn-portfolio": {
+        type: "web",
+        media: ["https://res.cloudinary.com/dmg2dllnz/image/upload/v1742821119/tp_0_budhvp.png",
+                "https://res.cloudinary.com/dmg2dllnz/image/upload/v1742821120/tp_1_a1qemr.png",
+                "https://res.cloudinary.com/dmg2dllnz/image/upload/v1742821123/tp_2_t5q4ps.png",
+                "https://res.cloudinary.com/dmg2dllnz/image/upload/v1742821120/tp_3_qqbn5x.png",
+                "https://res.cloudinary.com/dmg2dllnz/image/upload/v1742821120/tp_4_y5ret3.png",
+                "https://res.cloudinary.com/dmg2dllnz/image/upload/v1742821120/tp_5_x2f1fn.png"],
+        width: 3840,
+        height: 2240,
+        alt: "Tonamn portfolio media",
+        source: "https://tonamn.github.io/tonamn-portfolio/"
+    },
+    "broked": {
+        type: "app",
+        media: ["https://youtube.com/shorts/HwFZy7ydrKM?feature=share"],
+        alt: "Broked demo",
+        source: null
+    }
+}
+
+export default projects
