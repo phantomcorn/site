ca6307ee77142c03ea7a48e2076f657acaa37b46
phantomcorn <jj.phantakorn@outlook.com>
2025-03-26 23:26:53 +0700

    fading slideshow

--- src/components/Slideshow/Slideshow.js
+++ src/components/Slideshow/Slideshow.js
@@ -1,6 +1,9 @@
 import { useState, useEffect} from "react"
 import styles from "./Slideshow.module.css"
 import Image from "next/image"
+
+import {motion, AnimatePresence} from "framer-motion"
+
 export default function Slideshow({src}) {
 
     const [imgIndex, setImgIndex] = useState(0)
@@ -15,17 +18,26 @@ export default function Slideshow({src}) {
         }
     },[])
 
+    useEffect(() => {
+
+    },[imgIndex])
+
     return (
         <div className={styles.slideshow}>
-            {src && src.media?.map((img, i) => 
-                <Image 
-                    className={`${styles.media} ${imgIndex == i ? styles.show : styles.hide}`} 
-                    key={src.alt + (i + 1)}
-                    src={img} alt={src.alt} 
-                    width={src.width} 
-                    height={src.height}
-                />
-            )}
+            <AnimatePresence>
+                {src && src.media?.map((img, i) => 
+                    <motion.img 
+                        className={`${styles.media}`} 
+                        initial={{opacity: 0}}
+                        animate={{ opacity: i == imgIndex ? 1 : 0 }}
+                        exit={{ opacity: 0 }}
+                        key={src.alt + (i + 1)}
+                        src={img} alt={src.alt} 
+                        width={src.width} 
+                        height={src.height}
+                    />
+                )}
+            </AnimatePresence>
         </div>
     )
 }
