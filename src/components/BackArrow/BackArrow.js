30d96923f16ab2f7cef323dea669a88bb189deac
phantomcorn <jj.phantakorn@outlook.com>
2025-04-07 16:44:40 +0700

    camel case style

--- src/components/BackArrow/BackArrow.js
+++ src/components/BackArrow/BackArrow.js
@@ -2,8 +2,8 @@ import styles from "./BackArrow.module.css"
 export default function BackArrow() {
 
     return (
-        <svg className={styles["backarrow-svg"]} width="22" height="16" viewBox="0 0 22 16">
-            <path className={styles["backarrow-path"]}/>
+        <svg className={styles.backarrowSvg} width="22" height="16" viewBox="0 0 22 16">
+            <path className={styles.backarrowPath}/>
         </svg>
     )
 }
